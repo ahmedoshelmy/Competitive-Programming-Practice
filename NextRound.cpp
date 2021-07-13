e6b0cc621b852ea6febe48896b729dc0ce6887db
AhmedOsama198 <55411484+AhmedOsama198@users.noreply.github.com>
2021-07-13 11:23:14 +0200

    Create NextRound.cpp

---
+++ NextRound.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,k,cnt=0,x,val,t=1;
+    cin>>n>>k;
+
+
+for(int i=1;i<=n;i++){
+    cin>>x;
+    if(x>0 && i<=k){
+      cnt++;
+    }
+    if(i==k)val=x;
+    if(i>k && val==x&&val>0) cnt++;
+
+}
+
+cout<<cnt;
+
+}

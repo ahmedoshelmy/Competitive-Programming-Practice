c779bad20ed58d01dd84ff119f8b78a9f97cbd8b
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2021-10-05 07:49:02 +0200

    Update 810-D2-B Summer Sell Off.cpp

--- 810-D2-B Summer Sell Off.cpp
+++ 810-D2-B Summer Sell Off.cpp
@@ -7,36 +7,36 @@
 
     int main(){
         
-     ll n,f,k,l,ans=0;
+     ll n,f,ans=0;
      vector<pair<int,int>> d;
      cin>>n>>f;
+     ll k[n],l[n];
      for(int i=0;i<n;i++){
-         cin>>k>>l;
-         if(l<=k){
-             ans+=l;
+         cin>>k[i]>>l[i];
+         if(l[i]<=k[i]){
+             ans+=l[i];
          }else{
-             if(2*k>l){
-                 d.push_back(make_pair(l,k));
+             if(2*k[i]>l[i]){
+                 d.push_back(make_pair(l[i]-k[i],i));
              }else{
-                 d.push_back(make_pair(2*k,k));
+                 d.push_back(make_pair(k[i],i));
              }
          }
      }
-     for(int i=0;i<f;i++){
-              int maxdiff=0,index=0;;
-     for(int j=0;j<d.size();j++){
-         if(maxdiff<d[j].first-d[j].second){
-             maxdiff=d[j].first-d[j].second;
-             index=j;
-         }
-     }
-     if(maxdiff==0) break;
-     ans+=d[index].first;
-     d[index].first=d[index].second=0;
-     }
-     for(int j=0;j<d.size();j++){
-         ans+=d[j].second;
-     }
+    //      for(int i=0;i<d.size();i++){
+    //      cout<<d[i].first<<" "<<d[i].second<<endl;
+    //  }
+     sort(d.rbegin(),d.rend());
+    //  for(int i=0;i<d.size();i++){
+    //      cout<<d[i].first<<" "<<d[i].second<<endl;
+    //  }
+     for(int i=0;i<d.size();i++){
+             if(i>=f){
+                 ans+=k[d[i].second];
+             }else{
+                 ans+=(d[i].first+k[d[i].second]);
+             }
+     }  
      cout<<ans;
 
        return 0;

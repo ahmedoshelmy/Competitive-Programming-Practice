b863abdbc6566df1e19e0711c9fa05838cdd1f1d
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2021-10-09 17:23:35 +0200

    Create 747Div2 A. Consecutive Sum Riddle.cpp

---
+++ 747Div2 A. Consecutive Sum Riddle.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+     ll n,a,b,c,l,r;
+  cin>>n; 
+      while(n--){
+          bool no=true;
+          cin>>c;
+        cout<<-c+1<<" "<<c<<endl;
+      }
+}

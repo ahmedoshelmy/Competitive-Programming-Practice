6f309e434dd00fc04301300c1100bfd816c133d5
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2021-12-30 12:40:20 +0200

    Create Snakes and Ladders: The Quickest Way Up.cpp

---
+++ HackerRank/Snakes and Ladders: The Quickest Way Up.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'quickestWayUp' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. 2D_INTEGER_ARRAY ladders
+ *  2. 2D_INTEGER_ARRAY snakes
+ */
+
+
+
+int quickestWayUp(vector<vector<int>> ladders, vector<vector<int>> snakes) {
+    int grid[102]={0};
+    int stepArr[102]={0};
+    bool vis[102]={0};
+    vis[1] = 1;
+    queue<int> q;
+    int Cell = 1;
+    int curr;
+    map <int,int>moves;
+        q.push(Cell);
+    for (int i = 0; i < ladders.size(); ++i) {
+        grid[ladders[i][0]] = 1;
+        moves[ladders[i][0]] = ladders[i][1];
+    }
+    for (int i = 0; i < snakes.size(); ++i) {
+        grid[snakes[i][0]] = -1;
+        moves[snakes[i][0]] = snakes[i][1];
+    }
+    while(q.size()!=0&&Cell<100){
+        Cell = q.front();
+        q.pop();
+        for (int i = 1; i <=6 ; ++i) {
+                curr = Cell+i;
+            if(vis[curr])continue;
+            vis[curr] = 1;
+            if((grid[curr]==1||grid[curr]==-1)){
+                    curr = moves[curr];
+                }
+                stepArr[curr] = stepArr[Cell]+1;
+                if(curr>99) return stepArr[curr];
+                q.push(curr);
+        }
+
+    }
+    return  -1;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        vector<vector<int>> ladders(n);
+
+        for (int i = 0; i < n; i++) {
+            ladders[i].resize(2);
+
+            string ladders_row_temp_temp;
+            getline(cin, ladders_row_temp_temp);
+
+            vector<string> ladders_row_temp = split(rtrim(ladders_row_temp_temp));
+
+            for (int j = 0; j < 2; j++) {
+                int ladders_row_item = stoi(ladders_row_temp[j]);
+
+                ladders[i][j] = ladders_row_item;
+            }
+        }
+
+        string m_temp;
+        getline(cin, m_temp);
+
+        int m = stoi(ltrim(rtrim(m_temp)));
+
+        vector<vector<int>> snakes(m);
+
+        for (int i = 0; i < m; i++) {
+            snakes[i].resize(2);
+
+            string snakes_row_temp_temp;
+            getline(cin, snakes_row_temp_temp);
+
+            vector<string> snakes_row_temp = split(rtrim(snakes_row_temp_temp));
+
+            for (int j = 0; j < 2; j++) {
+                int snakes_row_item = stoi(snakes_row_temp[j]);
+
+                snakes[i][j] = snakes_row_item;
+            }
+        }
+
+        int result = quickestWayUp(ladders, snakes);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}

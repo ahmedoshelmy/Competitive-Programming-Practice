287f71b0d02fb92404e5f21edbfed72d8514864a
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2022-05-11 16:22:10 +0200

    Update Important Used function.cpp

--- Important Used function.cpp
+++ Important Used function.cpp
@@ -1,3 +1,32 @@
+    #include <bits/stdc++.h>
+    #include <algorithm>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+    using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+    using namespace __gnu_pbds;
+
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+    using namespace std;
+    using ll = long long;
+    #define pb push_back
+    #define Linguini ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #define cinarr     for (int i = 0; i < n; ++i) cin>>a[i]
+    #define coutarr for (int i = 0;i<n ; i++) cout<<a[i]<<" ";
+    #define coutarrn for (int i = 0;i<n ; i++) cout<<a[i]<<endl;
+    #define cingrid  for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) cin>>grid[i][j];
+    #define coutgrid  for (int i = 0; i < n; ++i){ for (int j = 0; j < m; ++j) cout<<grid[i][j];cout<<endl;}
+    #define lp(i, n)    for(int i=0;i<(int)(n);++i)
+    #define rep for(int i = 0;i<n;i++)
+    #define el endl
+    typedef vector<int> vi;
+    typedef vector<vi> vvi;
+    const long long MOD = 1e9+7;
+    const int N = 32768;
+
+
+
 long long gcd(long long int a, long long int b)
 {
     if (b == 0)

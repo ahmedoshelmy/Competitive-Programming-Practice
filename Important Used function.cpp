f39d48a9fdc238cac4353f3c168d82a438773270
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2022-05-11 14:34:21 +0200

    Create Important Used function.cpp

---
+++ Important Used function.cpp
@@ -0,0 +1,12 @@
+long long gcd(long long int a, long long int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ 
+// Function to return LCM of two numbers
+long long lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}

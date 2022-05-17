47282186600b07ecaac60ea804c3ecb2a775ccf4
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2022-05-17 08:56:37 +0200

    Update Important Used function.cpp

--- Important Used function.cpp
+++ Important Used function.cpp
@@ -1,4 +1,31 @@
-    #include <bits/stdc++.h>
+//DR Mostafa
+
+#define all(v)				((v).begin()), ((v).end())
+#define sz(v)				((int)((v).size()))
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define rep(i, v)		for(int i=0;i<sz(v);++i)
+#define lp(i, n)		for(int i=0;i<(int)(n);++i)
+#define lpi(i, j, n)	for(int i=(j);i<(int)(n);++i)
+#define lpd(i, j, n)	for(int i=(j);i>=(int)(n);--i)
+
+typedef long long         ll;
+const ll OO = 1e8;
+#define pb					push_back
+#define MP					make_pair
+#define P(x)				cout<<#x<<" = { "<<x<<" }\n"
+typedef long double   	  ld;
+typedef vector<int>       vi;
+typedef vector<double>    vd;
+typedef vector< vi >      vvi;
+typedef vector< vd >      vvd;
+typedef vector<string>    vs;
+
+
+
+
+
+
+#include <bits/stdc++.h>
     #include <algorithm>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>

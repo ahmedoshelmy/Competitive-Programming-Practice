1c3ac36d854e1f60db13322bdf967b5655dafb9f
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2022-07-06 20:39:25 +0200

    Create UVA 10302 Summation of Polynomials.cpp

---
+++ UVA/UVA 10302 Summation of Polynomials.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define pb push_back
+#define eb emplace_back
+
+#define Linguini ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define cinarr     for (int i = 0; i < n; ++i) cin>>arr[i]
+#define cin(vec) for(auto& i : vec) cin >> i;
+#define cout(vec) for(auto& i : vec) cout << i << " ";
+#define coutarr for (int i = 0;i<n ; i++) cout<<arr[i]<<" ";
+#define coutarrn for (int i = 0;i<n ; i++) cout<<arr[i]<<endl;
+#define cingrid  for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) cin>>grid[i][j];
+#define coutgrid  for (int i = 0; i < n; ++i){ for (int j = 0; j < m; ++j) cout<<grid[i][j];cout<<endl;}
+#define lp(i, n)    for(int i=0;i<(int)(n);++i)
+#define all(v) v.begin(),v.end()
+#define  NO re void(cout<<"NO\n");
+#define YES re void(cout<<"YES\n");
+#define rep for(int i = 0;i<n;i++)
+#define popcnt(a) __builtin_popcount(a)
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define re   			return
+#define el endl
+#define INF INT64_MAX
+#define error re void(cout<<-1<<el);
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+#define P pair<int ,int>
+typedef vector<vll> vvll;
+#define F first
+#define S second
+#define eps 1e-8
+const int N = 50003;
+
+ll gcd(ll a, ll b) { re(b == 0 ? a : gcd(b, a % b)); }
+ll lcm(ll a, ll b) { re((a*b) / gcd(a, b)); }
+
+int dx [] = {0,0,1,1};
+int dy [] = {0,1,0,1};
+bool isPrime(ll n) { if (n == 2)re 1; if (n<2 || n % 2 == 0)re 0; for (ll i = 3; i*i <= n; i += 2)if (n%i == 0)re 0; re 1; }
+ll Init[N];
+void solve () {
+ll x ;
+while(cin>>x) cout<<Init[x]<<el;
+}
+int main()
+{
+    Init[0] = 0;
+    for (ll i = 1; i <=N ; ++i) {
+        Init[i]=(i*i*i)+Init[i-1];
+    }
+//    Linguini
+    int t = 1;
+//    cin>>t;
+ while  (t--){
+        solve();
+    }
+    return  0;
+}

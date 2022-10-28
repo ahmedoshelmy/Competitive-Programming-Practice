08f47f04a6638f7605509811668b1f05795bbab5
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2022-10-28 10:48:54 +0200

    Create Minimizing Coins.cpp

---
+++ CSES/Dynamic Programming/Minimizing Coins.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define pb push_back
+#define eb emplace_back
+
+#define Linguini ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define cinarr     for (int i = 0; i < n; ++i) cin>>arr[i]
+#define cin(vec) for(auto& i : vec) cin >> i;
+#define cout(vec) for(auto& i : vec) cout << i << " ";
+#define coutarr for (int i = 0;i<n ; i++) cout<<arr[i]<<" ";
+#define coutarrn for (int i = 0;i<n ; i++) cout<<arr[i]<<endl;
+#define cingrid  for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) cin>>grid[i][j];
+#define coutgrid  for (int i = 0; i < n; ++i){ for (int j = 0; j < m; ++j) cout<<grid[i][j];cout<<endl;}
+#define lp(i, n)    for(int i=0;i<(int)(n);++i)
+#define all(v) v.begin(),v.end()
+#define  NO re void(cout<<"NO\n");
+#define YES re void(cout<<"YES\n");
+#define rep for(int i = 0;i<n;i++)
+#define popcnt(a) __builtin_popcount(a)
+#define clr(v, d)            memset(v, d, sizeof(v))
+#define re               return
+#define el endl
+const ll  INF = 1e17;
+#define error re void(cout<<-1<<el);
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+#define P pair<int ,int>
+typedef vector<vll> vvll;
+#define F first
+#define S second
+#define eps 1e-8
+const ll mod = 1e9+7;
+const int N = 100;
+
+int dx[]={1,0,-1,0,1,1,-1,-1}; // down - right - up - left - down_right - down-left - up _right - up_left
+int dy[]={0,1,0,-1,1,-1,1,-1};
+ll PowMod(ll bs, ll ex, ll M) { if (!ex)re 1; ll p = PowMod((bs*bs) % M, ex >> 1, M); re(ex & 1) ? (p*bs) % M : p; }
+
+
+int n,x ; vi a; vi memo ;
+int dp(int sum = 0 ){
+    if(sum > x) return 1e9 ;
+    if(sum==x)return  0 ;
+    int & ret = memo[sum] ;
+    if(~ret) re ret ;
+    ret = 1e9 +1;
+    for (int j = 0; j < n; ++j)  ret = min(ret , 1+dp(sum+a[j]));
+    return ret ;
+}
+
+
+void solve(){
+ cin>>n>>x ; a = vi(n) ; cin(a)  memo = vi(x+5,-1);
+ int ans = dp(0 ) ; if(ans > 1e9) ans = -1;
+ cout<<ans<<el;
+}
+int main(){
+//    Linguini
+    int t = 1;
+    while(t--){
+        solve();
+    }
+}

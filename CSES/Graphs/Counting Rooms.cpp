3054290534d651a39f6155a5bbb4ad16b9c180b6
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2022-12-08 07:24:32 +0200

    Create Counting Rooms.cpp

---
+++ CSES/Graphs/Counting Rooms.cpp
@@ -0,0 +1,76 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+
+    using ll = long long;
+    #define pb push_back
+    #define eb emplace_back
+
+    #define Linguini ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #define cinarr     for (int i = 0; i < n; ++i) cin>>arr[i]
+    #define cin(vec) for(auto& i : vec) cin >> i;
+    #define cout(vec) for(auto& i : vec) cout << i << " ";
+    #define coutarr for (int i = 0;i<n ; i++) cout<<arr[i]<<" ";
+    #define coutarrn for (int i = 0;i<n ; i++) cout<<arr[i]<<endl;
+    #define cingrid  for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) cin>>grid[i][j];
+    #define coutgrid  for (int i = 0; i < n; ++i){ for (int j = 0; j < m; ++j) cout<<grid[i][j];cout<<endl;}
+    #define lp(i, n)    for(int i=0;i<(int)(n);++i)
+    #define all(v) v.begin(),v.end()
+    #define  NO re void(cout<<"NO\n");
+    #define YES re void(cout<<"YES\n");
+    #define rep for(int i = 0;i<n;i++)
+    #define popcnt(a) __builtin_popcount(a)
+    #define clr(v, d)            memset(v, d, sizeof(v))
+    #define re               return
+    #define el endl
+    const ll  INF = 1e17;
+    #define error re void(cout<<-1<<el);
+    typedef vector<int> vi;
+    typedef vector<ll> vll;
+    typedef vector<vi> vvi;
+    #define P pair<int ,int>
+    typedef vector<vll> vvll;
+    #define F first
+    #define S second
+    #define ar array
+    const ll  eps  = 1e-17;
+    const ll mod = 1e9+7;
+    const ll N =2e5+2;
+
+    int dx[]={1,  0,-1,0,1,1,-1,-1}; // down - right - up - left - down_right - down-left - up _right - up_left
+    int dy[]={0, 1,0,-1,1,-1,1,-1};
+
+    int n , m ;
+    int vis[1001][1001];
+    vector<vector<char>> grid;
+    void dfs(int x , int y ){ 
+     vis[x][y]++ ;
+        for (int i = 0; i < 4; ++i) {
+            int nx = x+dx[i], ny = y + dy[i];
+            if(nx>=0 && nx<n && ny<m && ny>=0){
+                if(!vis[nx][ny]&& grid[nx][ny]=='.') dfs(nx , ny) ;
+            }
+        }
+    }
+    void solve(){
+     cin>>n>>m; char x;
+      grid =  vector<vector<char>>(n , vector<char> (m));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                cin>>grid[i][j];
+            }
+        }
+        int cnt = 0 ;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if(grid[i][j]=='.'&& !vis[i][j])dfs(i, j) , cnt++ ;
+            }
+        }
+        cout<<cnt<<el;
+    }
+    int main(){
+        Linguini
+        int t = 1;
+        while(t--){
+            solve();
+        }
+    }

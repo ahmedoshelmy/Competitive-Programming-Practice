3ed8207f01242682913c55adbfeef2a7b7fabfe7
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2022-12-22 16:09:43 +0200

    Update Number Theory .cpp

--- Number Theory .cpp
+++ Number Theory .cpp
@@ -1,3 +1,27 @@
+ 
+u64 binpower(u64 base, u64 e, u64 mod) {
+    u64 result = 1;
+    base %= mod;
+    while (e) {
+        if (e & 1)
+            result = (u128)result * base % mod;
+        base = (u128)base * base % mod;
+        e >>= 1;
+    }
+    return result;
+}
+ 
+bool check_composite(u64 n, u64 a, u64 d, int s) {
+    u64 x = binpower(a, d, n);
+    if (x == 1 || x == n - 1)
+        return false;
+    for (int r = 1; r < s; r++) {
+        x = (u128)x * x % n;
+        if (x == n - 1)
+            return false;
+    }
+    return true;
+};
 bool MillerRabin(u64 n, int iter=5) { // returns true if n is probably prime, else returns false.
     if (n < 4)
         return n == 2 || n == 3;
